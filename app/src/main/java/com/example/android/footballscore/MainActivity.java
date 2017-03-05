e4022f5d18ecd7f38a6bf263a51e1f9846af9a13
Max Riese <max@kleinerriese.de>
2017-03-05 22:41:01 +0100

    Hide titlebar

--- app/src/main/java/com/example/android/footballscore/MainActivity.java
+++ app/src/main/java/com/example/android/footballscore/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.footballscore;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
     }
 
